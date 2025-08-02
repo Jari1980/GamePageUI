b2661e6e0be2c3201bd33c171e8326dd0460a1cc
jari.tenhunen <jari.tenhunen@outlook.com>
2025-08-02 13:26:48 +0200

    Pong game added, compressed as Grotli using decompression falllback from unity to decompress

--- src/components/Pong.jsx
+++ src/components/Pong.jsx
@@ -1,11 +1,26 @@
-import React from 'react';
+import React from "react";
+import { Unity, useUnityContext } from "react-unity-webgl";
 
 const Pong = () => {
-    return (
-        <div>
-            <h1>Game Page</h1>
-        </div>
-    );
+  const { unityProvider } = useUnityContext({
+    loaderUrl: "Build/Pong.loader.js",
+    dataUrl: "Build/Pong.data.unityweb",
+    frameworkUrl: "Build/Pong.framework.js.unityweb",
+    codeUrl: "Build/Pong.wasm.unityweb",
+  });
+
+  return (
+    <>
+    <h1>Pong</h1>
+      <Unity 
+      unityProvider={unityProvider} 
+      style={{
+        height: 700,
+        width: 1000,
+      }}
+      />
+    </>
+  );
 };
 
 export default Pong;
